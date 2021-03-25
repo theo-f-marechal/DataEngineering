1d464f82d79550a582a03e147b36cb46ff7cd275
jean-94 <57668051+jean-94@users.noreply.github.com>
2021-03-25 17:54:32 +0100

    Update GeneratorRandomReport.scala
    Remove all the forbidden keyword : "For" : transform all the for loop into recursive method that create a defined number of Citizen and Report

--- GeneratorRandomReport.scala
+++ GeneratorRandomReport.scala
@@ -1,32 +1,47 @@
 import scala.collection.mutable.ListBuffer
 
+
+object GenerateRandomReport
+{
+  val random = scala.util.Random
+
+  def main(args: Array[String]): Unit = {
+    generateRandomReport()
+  }
+
+  def generateRandomReport(): Unit = {
+    val id = random.nextInt(100)
+    val position = (random.nextInt(50), random.nextInt(50))
+    val citizens = new ListBuffer[Citizen]()
+    val words = new ListBuffer[String]()
+    createCitizen(5, citizens)
+    createWord(5, words)
+    val wordsList = words.toList
+    val citizenList = citizens.toList
+    val report = new Report(id, position, citizenList, wordsList)
+    print(report.toString)
+  }
+
+  def createCitizen(range : Int, citizens : ListBuffer[Citizen]): Unit = {
+    if(range == 0){print("Citizens created")}
+    else {createCitizen(range-1, citizens += new Citizen(random.alphanumeric.take(3 + random.nextInt(5-3 + 1)).mkString, random.nextInt(100)))
+    }
+  }
+
+  def createWord(range : Int, words : ListBuffer[String]) : Unit = {
+    if(range == 0){print("Words created")}
+    else {createWord(range-1, words += random.alphanumeric.take(5 + random.nextInt(10-5 + 1)).mkString)}
+  }
+
+}
+
+
 class Citizen(var name : String, var peaceScore : Int) {
-  override def toString: String = 
+  override def toString: String =
     "(Name : " + name + " PeaceScore : " + peaceScore + ")"
 }
 
 class Report(var id : Int, var position : (Int,Int), var citizens : List[Citizen], var words : List[String]) {
-    override def toString: String =
-      "Report \n id : " + id + "\n Position : " + position + "\n Citizens : " + citizens.toString + "\n Words heard : " + words
+  override def toString: String =
+    "Report \n id : " + id + "\n Position : " + position + "\n Citizens : " + citizens.toString + "\n Words heard : " + words
 }
-
-def generateRandomReport() = {
-  val random = scala.util.Random
-  val id = random.nextInt(100)
-  val position = (random.nextInt(50), random.nextInt(50))
-  val citizens = new ListBuffer[Citizen]()
-  val words = new ListBuffer[String]()
-  val a = 0
-  for(a <- 1 to 3) {
-    citizens += new Citizen(random.alphanumeric.take(random.between(3,5)).mkString, random.nextInt(100))
-  }
-  for (a <- 1 to 5) {
-    words += random.alphanumeric.take(random.between(5,10)).mkString
-  }
-  val wordsList = words.toList
-  val citizenList = citizens.toList
-  val report = new Report(id, position, citizenList, wordsList)
-  print(report.toString)
-}
-
-generateRandomReport()
